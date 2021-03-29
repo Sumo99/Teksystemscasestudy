44b333764540b514f44d7c67b1875dc840976ee6
Sumangal Sinha Roy <mf0824se@metrostate.edu>
2021-03-29 13:31:28 -0500

    Wishlist now works with default book of 1984 by george orwell. Added JSON flattener to the dependencies.

--- src/main/java/com/teksystems/library/MainController.java
+++ src/main/java/com/teksystems/library/MainController.java
@@ -1,6 +1,11 @@
 package com.teksystems.library;
 
+import com.github.wnameless.json.flattener.JsonFlattener;
+import org.apache.tomcat.util.json.JSONParser;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.json.JsonParser;
+import org.springframework.boot.json.JsonParserFactory;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.domain.Sort;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
@@ -8,10 +13,14 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.web.client.RestTemplate;
 
 import java.security.Principal;
+import java.sql.Date;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 @Controller
 public class MainController {
@@ -27,6 +36,11 @@ public class MainController {
         this.amz = amz;
     }
 
+    @Bean
+    public RestTemplate restTemplate(RestTemplateBuilder builder) {
+        return builder.build();
+    }
+
     @RequestMapping(value = {"/", "/home"})
     public String index(Principal principal,Model model) {
         if(principal == null){
@@ -36,13 +50,39 @@ public class MainController {
         model.addAttribute("username",true);
         return "index";
     }
-
+    @RequestMapping(value = "/wishlist", method = RequestMethod.POST)
+    public String addBookToWishList(){
+        return "wishlist";
+    }
     @RequestMapping("/wishlist")
     public String wishlist(Principal principal, Model model){
+        RestTemplate restTemplate = new RestTemplate();
+        String book = restTemplate.getForObject("https://www.googleapis.com/books/v1/volumes?q=1984&maxResults=1",String.class);
+
+        Map<String, Object> map = JsonFlattener.flattenAsMap(book);
+
+        Book book1 = new Book();
+
+        book1.setIsbn((String) map.get("items[0].volumeInfo.industryIdentifiers[1].identifier"));
+        book1.setCover((String) map.get("items[0].volumeInfo.imageLinks.thumbnail"));
+        book1.setDescription((String) map.get("items[0].volumeInfo.description"));
+        book1.setCollection((String) map.get("items[0].volumeInfo.categories[0]"));
+        book1.setTitle((String) map.get("items[0].volumeInfo.title"));
+        book1.setLink((String) map.get("items[0].accessInfo.webReaderLink"));
+        book1.setPage_num((Integer) map.get("items[0].volumeInfo.pageCount"));
+        book1.setRating( Float.valueOf(map.get("items[0].volumeInfo.averageRating").toString()));
+        book1.setNum_ratings((Integer) map.get("items[0].volumeInfo.ratingsCount"));
+        book1.setPublisher((String) map.get("items[0].volumeInfo.publisher"));
+        book1.setPublished_date( new Date(Integer.valueOf((String)map.get("items[0].volumeInfo.publishedDate"))) );
+
+        System.out.println(book1);
         if(principal == null){
             model.addAttribute("username", false);
         }
+
         model.addAttribute("username",true);
+        model.addAttribute("book", book1);
+
         return "wishlist";
     }
 
@@ -68,6 +108,8 @@ public class MainController {
         model.addAttribute("fullString","C.GIF&client=hennp&type=xw12&oclc=");
         return "books";
     }
+
+
 
     private List<List<Book>> splitBooks(List<Book> allBooks){
         int rowSize = 4;
