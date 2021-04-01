e9984aff6a5e8088520605c79ce204276ba75fa6
Sumangal Sinha Roy <mf0824se@metrostate.edu>
2021-04-01 15:58:36 -0500

    Resolves the bug that password is always rejected.

--- src/main/java/com/teksystems/library/Users.java
+++ src/main/java/com/teksystems/library/Users.java
@@ -6,6 +6,7 @@ import javax.persistence.*;
 import java.util.List;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
 
 
@@ -14,15 +15,19 @@ import javax.validation.constraints.Size;
 
 public class Users {
     @Id
+
     @NotBlank(message = "Username is mandatory")
     @Size(min = 4, max = 250, message = "Enter a username between 4 and 250 characters")
+    @Pattern(regexp = "^[A-Za-z0-9-.@_]*$", message="No special characters for the username")
+
     private String username;
 
     @Size(min = 8, max = 250, message = "Enter an email address between 8 and 250 characters")
     @Email(message = "Enter a valid email address", regexp=".+@.+\\..+")
-    private String email;
+    @Pattern(regexp = "^[A-Za-z0-9-.@_]*$",message="No special characters for the email")
 
-    @Size(min = 8, max = 56, message = "Enter a password between 8 and 56 characters")
+    private String email;
+    //no need for password validations, since there is less to go wrong.
     private String password;
 
     @OneToMany
