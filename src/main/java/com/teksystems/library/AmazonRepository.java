f3452e391d0f98d904370c56d95fbb834fd37084
Sumangal Sinha Roy <mf0824se@metrostate.edu>
2021-04-05 12:10:24 -0500

    The python branch is only for python

--- src/main/java/com/teksystems/library/AmazonRepository.java
+++
@@ -1,9 +0,0 @@
-package com.teksystems.library;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface AmazonRepository extends JpaRepository<Amazon, Integer> {
-
-}
--- src/main/java/com/teksystems/library/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.teksystems.library;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<Users, String> {
-    Users findByUsername(String username);
-
-}
