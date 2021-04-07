d94b4b77c7103933a62a82ebf294957dcd0ee19f
Sumangal Sinha Roy <mf0824se@metrostate.edu>
2021-04-07 14:27:06 -0500

    Books are now customized per the user.

--- src/main/java/com/teksystems/library/Book.java
+++ src/main/java/com/teksystems/library/Book.java
@@ -2,9 +2,12 @@ package com.teksystems.library;
 
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.NamedNativeQueries;
+import org.hibernate.annotations.NamedNativeQuery;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 import java.sql.Date;
 
@@ -12,6 +15,9 @@ import java.sql.Date;
 @Entity
 @Table(name = "mroonga_books" )
 @NoArgsConstructor
+@NamedNativeQueries({
+        @NamedNativeQuery(name = "Book.findBooksForUser", query = "select * from user_books u JOIN mroonga_books m on u.books_id = m.id where users_username = ?1", resultClass = Book.class)
+})
 
 public class Book {
     @Id
@@ -34,4 +40,6 @@ public class Book {
         this.title = title;
     }
 
+    @ManyToOne
+    public Users user;
 }
--- src/main/java/com/teksystems/library/BookRepository.java
+++ src/main/java/com/teksystems/library/BookRepository.java
@@ -1,5 +1,7 @@
 package com.teksystems.library;
 
+import org.hibernate.annotations.NamedNativeQueries;
+import org.hibernate.annotations.NamedNativeQuery;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
@@ -10,4 +12,5 @@ import java.util.List;
 public interface BookRepository extends JpaRepository<Book, Integer> {
     List<Book> findBookByDescriptionContains(String query);
     List<Book> findBookByTitle(String title);
+    List<Book> findBooksForUser(String username);
 }
